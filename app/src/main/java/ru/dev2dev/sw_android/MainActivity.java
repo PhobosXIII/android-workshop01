f0bb6730f234ebf0d5cd8b6452fdc3b6aa32d986
PhobosXIII <phobosxiii@yandex.ru>
2016-01-27 21:23:15 +0700

    Add base activity

--- app/src/main/java/ru/dev2dev/sw_android/MainActivity.java
+++ app/src/main/java/ru/dev2dev/sw_android/MainActivity.java
@@ -1,9 +1,8 @@
 package ru.dev2dev.sw_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
