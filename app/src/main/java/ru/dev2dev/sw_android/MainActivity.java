21140dde7f07f942be82eb541e91c1aed2bb9ec2
PhobosXIII <phobosxiii@yandex.ru>
2016-02-07 20:10:18 +0700

    Moved to OkHttp library

--- app/src/main/java/ru/dev2dev/sw_android/MainActivity.java
+++ app/src/main/java/ru/dev2dev/sw_android/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,14 +12,16 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends BaseActivity {
     private ListView listView;
     private ProgressBar progressBar;
@@ -76,47 +77,29 @@ public class MainActivity extends BaseActivity {
         protected ArrayList<Person> doInBackground(Void... params) {
             Log.d(TAG, "AsyncTask doInBackground()");
 
-            BufferedReader reader = null;
+            String url = "http://swapi.co/api/people/";
+            OkHttpClient client = new OkHttpClient();
+
             try {
-                SystemClock.sleep(5000);
+                Request request = new Request.Builder()
+                        .url(url)
+                        .addHeader("User-Agent", "ws-sw-android-" + Build.VERSION.RELEASE)
+                        .build();
+                Response response = client.newCall(request).execute();
+                String body = response.body().string();
 
-                URL url = new URL("http://swapi.co/api/people/");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.addRequestProperty("User-Agent", "ws-sw-android-" + Build.VERSION.RELEASE);
-                connection.setRequestMethod("GET");
-                connection.connect();
-
-                int status = connection.getResponseCode();
-                if (status == 200) {
-                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String inputLine;
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    while ((inputLine = reader.readLine()) != null) {
-                        stringBuilder.append(inputLine);
-                    }
-
-                    JSONObject jsonObject = new JSONObject(stringBuilder.toString());
-                    JSONArray results = jsonObject.getJSONArray("results");
-                    ArrayList<Person> people = new ArrayList<>();
-                    for (int i = 0; i < results.length(); i++) {
-                        Person person = Person.fromJson(results.getJSONObject(i));
-                        people.add(person);
-                    }
-
-                    return people;
+                JSONObject jsonObject = new JSONObject(body);
+                JSONArray results = jsonObject.getJSONArray("results");
+                ArrayList<Person> people = new ArrayList<>();
+                for (int i = 0; i < results.length(); i++) {
+                    Person person = Person.fromJson(results.getJSONObject(i));
+                    people.add(person);
                 }
-            } catch (Exception e) {
+
+                return people;
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
-                return null;
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
 
             return null;
