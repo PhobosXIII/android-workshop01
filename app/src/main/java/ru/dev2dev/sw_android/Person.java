727b4ee50d3debcb0f04820e02fd7c2efe269e07
PhobosXIII <phobosxiii@yandex.ru>
2016-03-18 21:55:17 +0700

    Delete setters

--- app/src/main/java/ru/dev2dev/sw_android/Person.java
+++ app/src/main/java/ru/dev2dev/sw_android/Person.java
@@ -26,48 +26,24 @@ public class Person implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getHeight() {
         return height;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
     }
 
     public String getMass() {
         return mass;
     }
 
-    public void setMass(String mass) {
-        this.mass = mass;
-    }
-
     public String getEyeColor() {
         return eyeColor;
-    }
-
-    public void setEyeColor(String eyeColor) {
-        this.eyeColor = eyeColor;
     }
 
     public String getBirthYear() {
         return birthYear;
     }
 
-    public void setBirthYear(String birthYear) {
-        this.birthYear = birthYear;
-    }
-
     public String getGender() {
         return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
     }
 
     @Override
